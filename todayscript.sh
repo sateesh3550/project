3c04a7317eedadc241f4ac4ff09d57544b443181
root <root@testvm-sateesh.c.production-project-195021.internal>
2018-08-20 20:46:56 +0000

    updated code

---
+++ todayscript.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+#replace a sting in a file and copy that file to different directory or same
+#initial_key="xyz123"
+initial_key="$1"
+#final_key="dev123"
+final_key="$2"
+configfile="/root/class/configfile"
+target="/tmp/outfile"
+for l in configfile
+  do
+     if [ $l ]; then
+        #sed "s/$initial_key/$final_key/g" "$l" > $target && mv $target .
+        sed "s/$initial_key/$final_key/g"  > $target && mv $target .
+     else
+       echo "error"
+     fi
+  done
